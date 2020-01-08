3c62ad898213e9f0d4d7d0f111832f8811040fd6
inhobaek-wj <inhobaek.wj@gmail.com>
2020-01-09 04:44:41 +0900

    get query parameters

--- src/main/main.go
+++ src/main/main.go
@@ -8,7 +8,16 @@ import (
 )
 
 func yallo (c echo.Context) error {
-	return c.String(http.StatusOK, "yallo from the web site")
+        return c.String(http.StatusOK, "yallo from the web site")
+}
+
+func getCats (c echo.Context) error {
+        catName := c.QueryParam("name")
+        catType := c.QueryParam("type")
+
+        return c.String(
+                http.StatusOK,
+                fmt.Sprintf("Your cat name is %s and its type is %s\n", catName, catType))
 }
 
 func main() {
@@ -17,6 +26,6 @@ func main() {
         e := echo.New()
 
         e.GET("/", yallo)
-
+        e.GET("/cats", getCats)
         e.Start(":8080")
 }
