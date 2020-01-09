0a17ebebc846fea5f227694f02a6d554ae1f944d
inhobaek-wj <inhobaek.wj@gmail.com>
2020-01-09 18:10:52 +0900

    use cookie

--- src/main/main.go
+++ src/main/main.go
@@ -6,6 +6,8 @@ import (
         "io/ioutil"
         "log"
         "encoding/json"
+        "time"
+        "strings"
 
         "github.com/labstack/echo"  // go get github.com/labstack/echo
         "github.com/labstack/echo/middleware" // https://echo.labstack.com/middleware
@@ -47,8 +49,8 @@ func getCats (c echo.Context) error {
         if dataType == "json" {
                 return c.JSON(http.StatusOK,map[string]string {
                         "name": catName,
-			"type": catType,
-		})
+                                "type": catType,
+                        })
         }
 
         return c.JSON(http.StatusBadRequest, map[string]string{
@@ -113,13 +115,62 @@ func mainAdmin(c echo.Context) error {
         return c.String(http.StatusOK, "Hooray you are on the secret admin main page!")
 }
 
+func mainCookie(c echo.Context) error {
+        return c.String(http.StatusOK, "You are on the secret cookie page")
+}
+
+func login(c echo.Context) error {
+        username := c.QueryParam("username")
+        password := c.QueryParam("password")
+
+        // check username and password on DB after hasing the password.
+        if username == "inho" && password == "1234" {
+                cookie := &http.Cookie{}
+
+                // this is the same.
+                // coocke := new(http.Cookie)
+
+                cookie.Name = "sessionId"
+                cookie.Value = "some_string"
+                cookie.Expires = time.Now().Add(1 * time.Hour)
+
+                c.SetCookie(cookie)
+
+                return c.String(http.StatusOK, "You were logged in!")
+        }
+        return c.String(http.StatusOK, "Your username or password were wrong")
+}
+
 ////////////////// custom middlewares //////////////////
 func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		c.Response().Header().Set(echo.HeaderServer, "BlueBot/1.0")
-		c.Response().Header().Set("notReallyHeader", "thisHasNoMeaning")
-		return next(c)
-	}
+        return func(c echo.Context) error {
+                c.Response().Header().Set(echo.HeaderServer, "BlueBot/1.0")
+                c.Response().Header().Set("notReallyHeader", "thisHasNoMeaning")
+                return next(c)
+        }
+}
+
+func checkCookie(next echo.HandlerFunc) echo.HandlerFunc {
+        return func(c echo.Context) error {
+                cookie, err := c.Cookie("sessionId")
+
+                if err != nil {
+                        if strings.Contains(err.Error(),"named cookie not present") {
+                                return c.String(
+                                        http.StatusUnauthorized,
+                                        "You don't have the right cookie, cookie1")
+                        }
+			log.Println(err)
+			return err
+                }
+
+                if cookie.Value == "some_string" {
+                        return next(c)
+                }
+
+                return c.String(
+                        http.StatusUnauthorized, "You don't have the right cookie, cookie2")
+        }
 }
 
 func main() {
@@ -127,37 +178,48 @@ func main() {
 
         e := echo.New()
 
-	// apply custom middlewares.
-	e.Use(ServerHeader)
+        // apply custom middlewares.
+        e.Use(ServerHeader)
 
-	// add group.
-        g := e.Group("/admin")
-
-        // this logs the server interaction.
-        // g := e.Group("/admin", middleware.Logger())
-        // g.Use(middleware.Logger())
-        g.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-                Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
-        }))
-
-	// Authentication.
-	g.Use(middleware.BasicAuth(
-		func(username, password string, c echo.Context) (bool, error) {
-		// check in the DB.
-		if username == "inho" && password == "1234" {
-			return true, nil
-		}
-		return false, nil
-	}))
-
-        g.GET("/main", mainAdmin)
-
+        e.GET("/login", login)
         e.GET("/", yallo)
         e.GET("/cats/:data", getCats)
 
         e.POST("/cats", addCat)
         e.POST("/dogs", addDog)
         e.POST("/hamsters", addHamster)
+
+        // add admin group.
+        adminGroup := e.Group("/admin")
+
+        // this logs the server interaction.
+        // g := e.Group("/admin", middleware.Logger())
+        // g.Use(middleware.Logger())
+        adminGroup.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+                Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
+        }))
+
+        // Authentication.
+        adminGroup.Use(middleware.BasicAuth(
+                func(username, password string, c echo.Context) (bool, error) {
+                        // check in the DB.
+                        if username == "inho" && password == "1234" {
+                                return true, nil
+                        }
+                        return false, nil
+                }))
+
+        adminGroup.GET("/main", mainAdmin)
+
+        // add cookie group.
+        cookieGroup := e.Group("/cookie")
+
+        cookieGroup.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+                Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
+        }))
+        cookieGroup.Use(checkCookie)
+
+        cookieGroup.GET("/main", mainCookie)
 
         e.Start(":8080")
 }
